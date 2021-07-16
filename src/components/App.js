249e856fece2a3b3b14ae09e294473c118e25923
Marion-Lotito <marion.lotito@gmail.com>
2021-07-16 17:41:15 +0200

    add iscompleted value to the list

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,21 @@ import './App.css';
 function App() {
   
   const [todos, setTodos] = React.useState([
-    { text: "Jog around the park 3x" },
-    { text: "10 minutes meditation" },
-    { text: "Read for one hour" },
-    { text: "Pick up groceries" },
-    { text: "Complete Todo App on Frontend Mentor" }
+    { text: "Jog around the park 3x",
+      isCompleted: false 
+    },
+    { text: "10 minutes meditation",
+      isCompleted: false 
+    },
+    { text: "Read for one hour",
+      isCompleted: false 
+    },
+    { text: "Pick up groceries",
+      isCompleted: false 
+    },
+    { text: "Complete Todo App on Frontend Mentor",
+      isCompleted: false 
+    },
   ]);
   
   const addTodo = text => {
